6f5b4da63d1578c3a62853eaab84d63ef0f4d5d5
aneetan <anitaneupane2061@gmail.com>
2025-08-17 15:23:57 +0545

    fix: testmatch in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+   testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '!**/__tests__/**/*.empty.test.[jt]s?(x)' 
+  ],
   setupFilesAfterEnv: [], 
   globalSetup: undefined, 
   globalTeardown: undefined
