40a5ca573aade533623d0e0d64eb745cc63adc9e
aneetan <anitaneupane2061@gmail.com>
2025-08-17 15:20:46 +0545

    fix: updated jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  globalSetup: './jest.global-setup.ts',
-  globalTeardown: './jest.global-teardown.ts'
+  setupFilesAfterEnv: [], 
+  globalSetup: undefined, 
+  globalTeardown: undefined
 };
